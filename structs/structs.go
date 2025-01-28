b26c776968d9a0e076eae5b46491126a2df457c3
Saksham Saigal <saigalsaksham96@gmail.com>
2025-01-28 12:59:47 +0530

    SAKY-201 : Added struct project with example

---
+++ structs/structs.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName  string
+	birthdate string
+	createdAt time.Time
+}
+
+func main() {
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	// ... do something awesome with that gathered data!
+	// var userData user
+	userData := user{
+		firstName: userFirstName,
+		lastName:  userLastName,
+		birthdate: userBirthdate,
+		createdAt: time.Now(),
+	}
+	outputUserData(userData)
+}
+
+func outputUserData(u user) {
+	println(u.firstName, u.lastName, u.birthdate)
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
