c3b05ba587521d1a01c28899f4cfc8cd4638847f
Saksham Saigal <saigalsaksham96@gmail.com>
2025-01-30 14:01:18 +0530

    SAKY-204 : made fields in accsible from outside and methods exposed for a struct

--- structs/structs.go
+++ structs/structs.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"example/structs/user"
-	"fmt"
-	"time"
 )
 
 func main() {
-	userFirstName := getUserData("Please enter your first name: ")
-	userLastName := getUserData("Please enter your last name: ")
-	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+	userFirstName := user.GetUserData("Please enter your first name: ")
+	userLastName := user.GetUserData("Please enter your last name: ")
+	userBirthdate := user.GetUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
 	appUser, err := user.NewCreateUserValidation(userFirstName, userLastName, userBirthdate)
 
@@ -19,13 +17,13 @@ func main() {
 
 	// ... do something awesome with that gathered data!
 	// var userData user
-	userData := user.User{
-		FirstName: userFirstName,
-		LastName:  userLastName,
-		Birthdate: userBirthdate,
-		CreatedAt: time.Now(),
-	}
-	userData.OutputUserDataStructMethod()
+	// userData := user.User{
+	// 	firstName: userFirstName,
+	// 	lastName:  userLastName,
+	// 	birthDate: userBirthdate,
+	// 	createdAt: time.Now(),
+	// }
+	// userData.OutputUserDataStructMethod()
 	// outputUserData(userData) //normally using struct data in a function
 	// outputUserDataUsingPointer(&userData) // pointer to structs are passed
 	// userData.clearUserName()
@@ -48,20 +46,4 @@ func main() {
 
 	userData3.OutputUserDataStructMethod()
 
-}
-
-func outputUserData(u user.User) {
-	println(u.FirstName, u.LastName, u.Birthdate)
-}
-
-func outputUserDataUsingPointer(u *user.User) {
-	usr := *u
-	println(usr.FirstName, usr.LastName, usr.Birthdate)
-}
-
-func getUserData(promptText string) string {
-	fmt.Print(promptText)
-	var value string
-	fmt.Scanln(&value)
-	return value
 }
--- structs/user/user.go
+++ structs/user/user.go
@@ -2,40 +2,41 @@ package user
 
 import (
 	"errors"
+	"fmt"
 	"time"
 )
 
 type User struct {
-	FirstName string
-	LastName  string
-	Birthdate string
-	CreatedAt time.Time
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
 }
 
 func (u User) OutputUserDataStructMethod() {
-	println(u.FirstName, u.LastName, u.Birthdate)
+	println(u.firstName, u.lastName, u.birthDate)
 }
 
 func (u *User) ClearUserName() { // Passing pointer in this case so that the same value is edited not the copy of the struct object
-	u.FirstName = "" // shortcut syntax do not need to dreference the pointer, automatically done by go.
-	u.LastName = ""
+	u.firstName = "" // shortcut syntax do not need to dreference the pointer, automatically done by go.
+	u.lastName = ""
 }
 
 func NewCreateUser(firstName, lastName, birthdate string) User { //Constructor Function in go
 	return User{
-		FirstName: firstName,
-		LastName:  lastName,
-		Birthdate: birthdate,
-		CreatedAt: time.Now(),
+		firstName: firstName,
+		lastName:  lastName,
+		birthDate: birthdate,
+		createdAt: time.Now(),
 	}
 }
 
 func NewCreateUserPointer(firstName, lastName, birthdate string) *User { //Constructor Function using pointer in go, that created only one copy for argumants and return types
 	return &User{
-		FirstName: firstName,
-		LastName:  lastName,
-		Birthdate: birthdate,
-		CreatedAt: time.Now(),
+		firstName: firstName,
+		lastName:  lastName,
+		birthDate: birthdate,
+		createdAt: time.Now(),
 	}
 }
 
@@ -44,10 +45,26 @@ func NewCreateUserValidation(firstName, lastName, birthdate string) (*User, erro
 		return nil, errors.New("firstName, lastName and birthDate is required!")
 	}
 	return &User{
-		FirstName: firstName,
-		LastName:  lastName,
-		Birthdate: birthdate,
-		CreatedAt: time.Now(),
+		firstName: firstName,
+		lastName:  lastName,
+		birthDate: birthdate,
+		createdAt: time.Now(),
 	}, nil
 
+}
+
+func outputUserData(u User) {
+	println(u.firstName, u.lastName, u.birthDate)
+}
+
+func outputUserDataUsingPointer(u *User) {
+	usr := *u
+	println(usr.firstName, usr.lastName, usr.birthDate)
+}
+
+func GetUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scanln(&value)
+	return value
 }
