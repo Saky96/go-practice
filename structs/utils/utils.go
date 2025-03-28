c0665f17c161de3bc55fbbd2778cf28f4710c9c5
Saksham Saigal <saigalsaksham96@gmail.com>
2025-03-29 01:17:56 +0530

    SAKY-801 : Added notes project and file handeling

---
+++ structs/utils/utils.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+// Function to get user input
+func GetUserInput(prompt string) string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print(prompt)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Remove trailing newline characters
+	return strings.TrimSpace(input)
+}
+
+func SanitizeFileName(filename string) string {
+	filename = strings.ReplaceAll(filename, " ", "_")
+	filename = strings.ToLower(filename)
+	return filename
+}
